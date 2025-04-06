fd4709715e09f7cd7057947f5580c5c02d8092c2
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-04-06 17:41:46 +0530

    Regex in python,Argparser,Vertual Environment

---
+++ Session-10/task-2.py
@@ -0,0 +1,10 @@
+
+num1= float(input("Enter number 1: "))
+num2= float(input("Enter number 2: "))
+
+#display the input
+
+print("Total: ",num1+num2)
+
+# WAP to Validate Mobilenumber using Regex And Input
+
---
+++ Session-10/userinput.py
@@ -0,0 +1,10 @@
+
+name= input("Enter Your Name:")
+age= int(input("Enter Your Age:"))
+
+#display the input
+print("Hello,",name+" !")
+print("Your are",age+ " Years Old!")
+
+
+#WAP to take two input from the user and add them like a simple calculator
