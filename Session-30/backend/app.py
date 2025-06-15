2d4e59371b53b33836240e1913af576e962b83a3
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-06-15 16:08:55 +0530

    Docker Compose

--- Session-30/backend/app.py
+++ Session-30/backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
-app= Flask(__name__)
-
+app = Flask(__name__)
+CORS(app) 
 @app.route('/api/message')
 def message():
-    return jsonify({'message':'Hello From Flask Backend'})
+    return jsonify({'message': 'Hello from Flask backend!'})
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
